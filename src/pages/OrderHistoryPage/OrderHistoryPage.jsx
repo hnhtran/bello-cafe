b5ef29dfb5f2677b9d4c4e94c121e979ba5b3f6d
hnhtran <tran_eetas@yahoo.com>
2022-09-27 01:27:55 +1000

    Order History Page init

---
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -0,0 +1,11 @@
+import './OrderHistoryPage.css'
+
+const AuthPage = () => {
+    return (
+        <>
+            <h1>Order History Page</h1>
+        </>
+    )
+}
+
+export default OrderHistoryPage
