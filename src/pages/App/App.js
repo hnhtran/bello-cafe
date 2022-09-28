d9cc302d6e8c56c4fe681eb65cf7a7af59c98dd0
hnhtran <tran_eetas@yahoo.com>
2022-09-28 23:15:38 +1000

    navbar linked app js

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,18 +2,23 @@ import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import Navbar from '../../components/Navbar/Navbar';
 import { Routes, Route } from 'react-router-dom'
-
+import { useState } from 'react';
 
 function App() {
-  const user = 'null';
+  // const [user, setuser] = useState({})
+  const user = 'null'
   return (
     <main className="App">
       { user ? 
+      <>
+      <Navbar/>
       <Routes>
         <Route path="/orders/new" element={<NewOrderPage />} />
         <Route path="/orders/history" element={<OrderHistoryPage />} />
       </Routes>
+      </>
       : 
       <AuthPage /> }
     </main>
