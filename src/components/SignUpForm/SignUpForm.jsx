1e61302d3735b021ba2b8cca901662d9d111c299
hnhtran <tran_eetas@yahoo.com>
2022-09-30 01:32:05 +1000

    sign up form init

---
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class SignUpForm extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Sign Up</h1>
+            </div>
+        )
+    }
+}
+
+export default SignUpForm;
