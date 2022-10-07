4abff88903178603a0adaa59e701fee23c57c0ab
hnhtran <tran_eetas@yahoo.com>
2022-10-07 16:03:03 +1100

    crud-helper to check user model

--- crud-helper.js
+++ crud-helper.js
@@ -4,9 +4,9 @@ require('./config/database')
 
 // require Mongoose models
 const User = require('./models/user')
-const Item = require('./models/item')
-const Category = require('./models/category')
-const Order = require('./models/order')
+//const Item = require('./models/item')
+//const Category = require('./models/category')
+//const Order = require('./models/order')
 
 // local variables
 let user, item, category, Order
--- models/user.js
+++ models/user.js
@@ -40,4 +40,4 @@ userSchema.pre('save', async function (next) {
     return next();
 })
 
-module.exports = mongoose.model('User', 'userSchema')
+module.exports = mongoose.model('User', userSchema)
