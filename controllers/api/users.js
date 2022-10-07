deadf80d4961e40df049dca6007c43a2db6d96b7
hnhtran <tran_eetas@yahoo.com>
2022-10-07 16:15:15 +1100

    jwt complete

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -7,6 +7,8 @@ module.exports = {
 async function create(req, res) {
     try {
         const user = await User.create(req.body)
+        const token = createJWT(user)
+        res.json({ token })
     }
     catch (err) {
         res.status(400).json(err)
