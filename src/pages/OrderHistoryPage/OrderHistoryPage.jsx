ef1dcb8bc6350b18332e3cf17c8a95c8990dd983
hnhtran <tran_eetas@yahoo.com>
2022-10-12 21:25:52 +1100

    check token alert

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,9 +1,14 @@
 import './OrderHistoryPage.css'
 
 const OrderHistoryPage = () => {
+    const handleCheckToken = async () => {
+        alert('clicked')
+    }
+
     return (
         <>
             <h1>Order History Page</h1>
+            <button onClick={handleCheckToken}>Check when my login expires</button>
         </>
     )
 }
