85392b3b54b4062b2d905bd695ed8effcb40de21
hnhtran <tran_eetas@yahoo.com>
2022-10-14 12:39:25 +1100

    model itemSchema

---
+++ models/itemSchema.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    emoji: String,
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+},
+{
+    timestamps: true,
+})
+
+module.exports = itemSchema
