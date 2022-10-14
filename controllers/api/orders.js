27e4f27fd78d04f24a875f55064e7f3ed75f8c8e
hnhtran <tran_eetas@yahoo.com>
2022-10-14 12:54:01 +1100

    orders controllers skeleton

---
+++ controllers/api/orders.js
@@ -0,0 +1,13 @@
+const Item = require('../models/item');
+
+module.exports = {
+    cart,
+    addToCart,
+    setItemQtyInCart,
+    checkout
+}
+
+async function cart(req, res) {}
+async function addToCart(req, res) {}
+async function setItemQtyInCart(req, res) {}
+async function checkout(req, res) {}
