c89c3a763e7dd8fd924b30201e7e93b028fadbf5
hnhtran <tran_eetas@yahoo.com>
2022-10-21 01:03:42 +1100

    logo jsx complete

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,1 +1,12 @@
 import './Logo.css'
+
+export default function Logo() {
+    return (
+        <div className="Logo">
+            <span className="emoji">🍔</span>
+            <span className="text">Burger Joint</span>
+            <div>BELLO</div>
+            <div>CAFE</div>
+        </div>
+    )
+}
