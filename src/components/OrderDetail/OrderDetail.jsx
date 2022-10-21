febf5285a104c1aa2cdba822020a948461fbf202
hnhtran <tran_eetas@yahoo.com>
2022-10-21 20:16:17 +1100

    order detail jsx complete

--- src/components/OrderDetail/OrderDetail.jsx
+++ src/components/OrderDetail/OrderDetail.jsx
@@ -1,1 +1,43 @@
-import './OrderDetail.css'
+import './OrderDetail.css';
+import LineItem from '../LineItem/LineItem';
+
+export default function OrderDetail({ order }) {
+    if (!order) return null
+
+    const lineItems = order.lineItems.map(item =>
+        <LineItem key={item.id} lineItem={item} isPaid={order.isPaid}/>
+        )
+
+    return (
+        <div className="OrderDetail">
+            <div className="section-heading">
+                {
+                    order.isPaid ? 
+                    <span>ORDER <span className="smaller">{order.orderId}</span></span>
+                    :
+                    <span>NEW ORDER</span>
+                }
+                <span>{new Date(order.updatedAt).toLocaleDateString()}</span>
+            </div>
+            <div className="line-item-container flex-ctr-ctr flex-col scroll-y">
+                {
+                    lineItems.length ?
+                    <>
+                    {lineItems}
+                    <section className="total">
+                        {order.isPaid ? 
+                        <span className="right">TOTAL&nbsp;&nbsp;</span>
+                        : 
+                        <button className="btn-sm" onClick={() => alert('clicked')} disabled={!lineItems.length}>CHECKOUT</button>
+                        }
+                        <span>{order.totalQty}</span>
+                        <span className="right">${order.orderTotal.toFixed(2)}</span>
+                    </section>
+                    </>
+                    :
+                    <div className="hungry">Hungry?</div>
+                }
+            </div>
+        </div>
+    )
+}
