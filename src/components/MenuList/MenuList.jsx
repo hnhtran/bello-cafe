407555574916e12cca015de46ed911aa3cefe0fe
hnhtran <tran_eetas@yahoo.com>
2022-10-21 18:43:02 +1100

    menulist jsx complete

--- src/components/MenuList/MenuList.jsx
+++ src/components/MenuList/MenuList.jsx
@@ -1,1 +1,11 @@
 import './MenuList.css'
+import MenuListItem from '../MenuListItem/MenuListItem'
+
+export default function MenuList({ menuItems }) {
+    const items = menuItems.map(item => 
+        <MenuListItem key={item.id} menuItem={item} />
+    )
+    return (
+        <main className='MenuList'>{items}</main>
+    )
+}
