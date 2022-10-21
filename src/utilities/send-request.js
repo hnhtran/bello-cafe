077bdbe7ee66e2868e6e3035adb14ec271c69783
hnhtran <tran_eetas@yahoo.com>
2022-10-21 21:21:08 +1100

    send request complete

---
+++ src/utilities/send-request.js
@@ -0,0 +1,18 @@
+import { getToken } from './users-service';
+
+async function sendRequest(url, method = 'GET', payload = null) {
+    const options = { method };
+    if (payload) {
+        options.headers = { 'Content-Type': 'application/json' };
+        options.body = JSON.stringify(payload);
+    }
+    const token = getToken();
+    if (token) {
+        options.headers = options.headers || {};
+        options.headers.Authorization = `Bearer ${token}`;
+    }
+    const res = await fetch(url, options);
+    const data = await res.json();
+    if (res.ok) return data;
+    throw new Error(data.errMsg);
+}
--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,4 +1,4 @@
-import { getToken } from './users-service';
+import sendRequest from './send-request';
 
 const BASE_URL = '/api/users';
 
@@ -12,22 +12,4 @@ export function login(credentials) {
 
 export function checkToken() {
     return sendRequest(`${BASE_URL}/check-token`);
-}
-
-//====== Helper Functions ======//
-async function sendRequest(url, method = 'GET', payload = null) {
-    const options = { method };
-    if (payload) {
-        options.headers = { 'Content-Type': 'application/json' };
-        options.body = JSON.stringify(payload);
-    }
-    const token = getToken();
-    if (token) {
-        options.headers = options.headers || {};
-        options.headers.Authorization = `Bearer ${token}`;
-    }
-    const res = await fetch(url, options);
-    const data = await res.json();
-    if (res.ok) return data;
-    throw new Error(data.errMsg);
 }
