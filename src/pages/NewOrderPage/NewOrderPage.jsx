5c6d801fbd06d276074203008448d670097423cc
hnhtran <tran_eetas@yahoo.com>
2022-10-31 00:52:41 +1100

    user log out init

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,4 +1,11 @@
 import './NewOrderPage.css'
+import { useState, useEffect, useRef } from 'react'
+import * as itemsAPI from '../../utilities/items-api'
+import { Link } from 'react-router-dom'
+import Logo from '../../components/Logo/Logo'
+import MenuList from '../../components/MenuList/MenuList'
+import OrderDetail from '../../components/OrderDetail/OrderDetail'
+import CategoryList from '../../components/CategoryList/CategoryList'
 
 const NewOrderPage = () => {
     return (
