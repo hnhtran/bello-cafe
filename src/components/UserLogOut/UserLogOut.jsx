4c1c76fd413d6d4cff48fe0d739fdfcc15b039e4
hnhtran <tran_eetas@yahoo.com>
2022-10-31 00:56:46 +1100

    userlogout complete

--- src/components/UserLogOut/UserLogOut.jsx
+++ src/components/UserLogOut/UserLogOut.jsx
@@ -1,1 +1,17 @@
 import './UserLogOut.css'
+import { logOut } from '../../utilities/users-service'
+
+export default function UserLogOut({ user, setUser }) {
+    function handleLogOut() {
+        logOut()
+        setUser(null)
+    }
+
+    return (
+        <div className="UserLogOut">
+            <div>{user.name}</div>
+            <div className="email">{user.email}</div>
+            <button className="btn-sm" onClick={handleLogOut}>Log Out</button>
+        </div>
+    )
+}
