37bf5fba20aeb4507f3d84590443fe621d7dcc71
hnhtran <tran_eetas@yahoo.com>
2022-10-31 00:25:48 +1100

    import Logo

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -2,14 +2,15 @@ import './AuthPage.css'
 import SignUpForm from '../../components/SignUpForm/SignUpForm'
 import LoginForm from '../../components/LoginForm/LoginForm'
 import { useState } from 'react';
+import Logo from '../../components/Logo/Logo'
 
 const AuthPage = ({user, setUser}) => {
     const [showSignUp, setShowSignUp] = useState(true)
     return (
-        <>
+        <main className='AuthPage'>
             <button onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
             {showSignUp ? <SignUpForm setUser={setUser}/> : <LoginForm setUser={setUser}/>}
-        </>
+        </main>
     )
 }
 
