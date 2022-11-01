9599db0955f1b80dc255524cda150035d355b951
hnhtran <tran_eetas@yahoo.com>
2022-11-01 22:53:47 +1100

    order schema

--- models/order.js
+++ models/order.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const orderSchema = new Schema({},
+const orderSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    lineItems: [lineItemsSchema],
+    isPaid: { type: Boolean, default: false },
+},
     {
         timestamps: true,
     }
