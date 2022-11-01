cd19c6a889fa6935073e1b62e5bf300a47969511
hnhtran <tran_eetas@yahoo.com>
2022-11-01 22:56:40 +1100

    line item schema

--- models/order.js
+++ models/order.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const lineItemsSchema = new Schema({
+    qty: { type: Number, default: 1 },
+    item: itemSchema
+},
+{
+    timestamps: true,
+}
+)
+
 const orderSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User' },
     lineItems: [lineItemsSchema],
