6ea7906e3974bf5a6a4026e188dc4e98043b3f7b
hnhtran <tran_eetas@yahoo.com>
2022-11-01 22:57:56 +1100

    require itemSchema

--- models/order.js
+++ models/order.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const itemSchema = require('./itemSchema')
 
 const lineItemsSchema = new Schema({
     qty: { type: Number, default: 1 },
