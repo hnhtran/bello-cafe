9e5db30616f979a18d945ac9486c86ad5423dd68
hnhtran <tran_eetas@yahoo.com>
2022-11-01 23:25:50 +1100

    getCart

--- controllers/api/orders.js
+++ controllers/api/orders.js
@@ -8,7 +8,10 @@ module.exports = {
     checkout
 }
 
-async function cart(req, res) {}
+async function cart(req, res) {
+    const cart = await Order.getCart(req.user._id);
+    res.json(cart);
+}
 async function addToCart(req, res) {}
 async function setItemQtyInCart(req, res) {}
 async function checkout(req, res) {}
